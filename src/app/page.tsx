89a25244e963100465f32bc03aa647d9b5d12563
Chris Andrews <andrewsc33@icloud.com>
2023-03-11 15:40:52 -0800

    prisma init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <div>
+    <div className="w-screen h-screen overflow-y-auto">
+      <div className="flex flex-col items-center justify-center h-full w-full">
       <h1>My page</h1>
       <p>My page content</p>
+      </div>
     </div>
   )
 }
