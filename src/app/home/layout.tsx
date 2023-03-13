f48b5e6ee1c68b3c8d6945e8cb4e1214f5dcec13
Chris Andrews <andrewsc33@icloud.com>
2023-03-13 11:32:16 -0700

    layout

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,11 +1,12 @@
 import GlassPane from "@/components/Glasspane"
 import Navbar from "@/components/Navbar"
-
+import Sidebar from "@/components/Sidebar"
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen min-w-screen items-center">
       <Navbar />
-      <GlassPane className="w-full h-full"> 
+      <GlassPane className="w-full h-full flex "> 
+        <Sidebar />
       {children}
       </GlassPane>
     </div>
